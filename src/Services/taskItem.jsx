9775f1a665b23df152baa7420132504290e712bf
MzzuMrz <maruzzamatias@gmail.com>
2023-10-02 01:07:07 -0300

    V1.2: Styles fix

--- src/Services/taskItem.jsx
+++ src/Services/taskItem.jsx
@@ -5,7 +5,7 @@ import { db } from "../firebaseConfig";
 
 const TaskItem = ({ task, onDelete }) => {
   const [completed, setCompleted] = useState(task.completed);
-
+  const [isDeleting, setIsDeleting] = useState(false);
   const handleUpdate = async () => {
     try {
       setCompleted(!completed);
@@ -18,25 +18,44 @@ const TaskItem = ({ task, onDelete }) => {
     }
   };
 
+  const handleDelete = () => {
+    try {
+      setIsDeleting(true);
+      onDelete(task.id);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setIsDeleting(false);
+    }
+  };
+
   return (
-    <div className="flex items-center space-x-4 p-2 hover:bg-gray-100 transition duration-300">
-      <input
-        type="checkbox"
-        checked={completed}
-        onChange={handleUpdate}
-        className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-      />
-      <span
-        className={`flex-1 ${completed ? "line-through text-gray-400" : ""}`}
-      >
-        {task.task}
-      </span>
-      <button
-        onClick={() => onDelete(task.id)}
-        className="text-red-500 hover:text-red-700 transition duration-300"
-      >
-        Delete
-      </button>
+    <div>
+      {isDeleting ? (
+        <p>Borrando...</p>
+      ) : (
+        <div className="flex items-center space-x-4 p-2 hover:bg-gray-100 transition duration-300">
+          <input
+            type="checkbox"
+            checked={completed}
+            onChange={handleUpdate}
+            className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+          />
+          <span
+            className={`flex-2 ${
+              completed ? "line-through text-gray-400" : ""
+            }`}
+          >
+            {task.task}
+          </span>
+          <button
+            onClick={handleDelete}
+            className="p-2 bg-red-500 text-white rounded-lg focus:outline-none focus:bg-blue-600 hover:bg-blue-600 transition duration-300"
+          >
+            Delete
+          </button>
+        </div>
+      )}
     </div>
   );
 };
